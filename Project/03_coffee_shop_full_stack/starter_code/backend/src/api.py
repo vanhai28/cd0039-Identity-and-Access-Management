8846867c8c8b1febc5661d817d9ca2ccde457671
nguyễn văn hải <nvhaik18@gmail.com>
2022-07-21 00:28:23 +0700

    done project

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -1,16 +1,35 @@
 import os
-from flask import Flask, request, jsonify, abort
+from flask import Flask, Request, request, jsonify, abort
 from sqlalchemy import exc
 import json
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+import werkzeug
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
-
+db = SQLAlchemy()
 app = Flask(__name__)
 setup_db(app)
 CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+
+def convertRowToObject( row):
+    result = {}
+    for column in row.__table__.columns:
+      result[column.name] = getattr(row, column.name)
+    
+    return result
+
+def convertTableToList( table):
+    result = []
+    for row in table:
+        rowObj = convertRowToObject(row)
+        result.append(rowObj)
+    
+    return result
+    
 '''
 @TODO uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
@@ -28,8 +47,25 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['GET'])
+@cross_origin()
+def getDrinks():
+    try:
+        data = Drink.query.all()
+        listDrinks = convertTableToList(data)
 
+        drinks = []
 
+        for dict_drink in listDrinks:
+            drink = Drink(id=dict_drink['id'],title=dict_drink["title"], recipe=dict_drink["recipe"])
+            drinks.append(drink.short())
+
+        return jsonify({
+            'success':True,
+            "drinks": drinks
+        })
+    except:
+        abort(500)
 '''
 @TODO implement endpoint
     GET /drinks-detail
@@ -38,8 +74,26 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks-detail', methods=['GET'])
+@cross_origin()
+@requires_auth("get:drinks-detail")
+def getDrinksDetail(self):
+    try:
+        data = Drink.query.all()
+        listDrinks = convertTableToList(data)
 
+        customList = []
 
+        for dict_drink in listDrinks:
+            drink = Drink(id=dict_drink['id'],title=dict_drink["title"], recipe=dict_drink["recipe"])
+            customList.append(drink.long())
+
+        return jsonify({
+            'success':True,
+            "drinks": customList
+        })
+    except:
+        abort(500)
 '''
 @TODO implement endpoint
     POST /drinks
@@ -49,8 +103,18 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['POST'])
+@cross_origin()
+@requires_auth("post:drinks")
+def addDrinks(self):
+    try:
+        data = request.get_json()
 
-
+        drink = Drink(title=data["title"], recipe=json.dumps(data["recipe"]))
+        drink.insert()
+        return {"ok": True, "drinks": drink}
+    except:
+        abort(500)
 '''
 @TODO implement endpoint
     PATCH /drinks/<id>
@@ -62,7 +126,27 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<id>', methods=['PATCH'])
+@cross_origin()
+@requires_auth("patch:drinks")
+def patchDrinks(self, id):
+    try:
+        drink = Drink.query.filter(Drink.id == id).one_or_none()
 
+        if drink is None:
+            abort(404)
+            return
+        
+        data = request.get_json()
+        drink.recipe=json.dumps(data['recipe'])
+        drink.title=data['title']
+        drink.update()
+
+        return jsonify({
+            "success": True, "drinks": drink.long()
+        })
+    except:
+        abort(500)
 
 '''
 @TODO implement endpoint
@@ -74,7 +158,20 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
-
+@app.route('/drinks/<id>', methods=['DELETE'])
+@cross_origin()
+@requires_auth("delete:drinks")
+def removeDrinks(self,id):
+    try:
+        drink = Drink.query.filter(Drink.id == id).one_or_none()
+        if drink is None:
+            abort(404)
+            return
+        
+        drink.delete()
+        return jsonify({"success": True, "delete": id})
+    except:
+        abort(500)
 
 # Error Handling
 '''
@@ -106,9 +203,20 @@ def unprocessable(error):
 @TODO implement error handler for 404
     error handler should conform to general task above
 '''
-
-
+@app.errorhandler(404)
+def handle_not_found_error(e):
+    return jsonify({
+                "success": False,
+                "error": 404,
+                "message": "resource not found"
+                }), 404
+app.register_error_handler(404, handle_not_found_error)
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'dev-82fpdm3y.us', // the auth0 domain prefix
+    audience: 'https:/test/api', // the audience set for the auth0 app
+    clientId: 'qaKQ1I9M8tX67Dg1Ygu7YMvojNvVbuDp', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:4200', // the base url of the running ionic application. 
   }
 };
